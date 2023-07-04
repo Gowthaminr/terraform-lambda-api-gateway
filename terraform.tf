509b4b96dd3821963533f862704b53fc0ed66bf1
Gowthaminr <84511384+Gowthaminr@users.noreply.github.com>
2023-07-04 14:20:18 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
-provider "aws" {
+/* provider "aws" {
   region = var.aws_region
   access_key = "AKIA5BMGLEQ5VPTWYFT2"
   secret_key = "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"
-}
+} */
 
 
 
@@ -14,6 +14,8 @@ terraform {
   cloud {
     workspaces {
       name = "learn-terraform-lambda-api-gateway"
+      access_key = "AKIA5BMGLEQ5VPTWYFT2"
+      secret_key = "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"
     }
   }
 
