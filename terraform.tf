164ac1b079b6926db2825681541fd1900002ecf7
Gowthaminr <84511384+Gowthaminr@users.noreply.github.com>
2023-07-04 14:18:36 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,11 @@
+provider "aws" {
+  region = var.aws_region
+  access_key = "AKIA5BMGLEQ5VPTWYFT2"
+  secret_key = "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"
+}
+
+
+
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
