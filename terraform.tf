fb11d843b56b9e4a48fdd715eb7710fce2b97219
Gowthaminr <84511384+Gowthaminr@users.noreply.github.com>
2023-07-04 14:24:56 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,21 +1,12 @@
-provider "aws" {
-  region = var.aws_region
-  access_key = "AKIA5BMGLEQ5VPTWYFT2"
-  secret_key = "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"
-} 
-
-
-
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-/* terraform {
+terraform {
 
   cloud {
     workspaces {
       name = "learn-terraform-lambda-api-gateway"
-      access_key = "AKIA5BMGLEQ5VPTWYFT2"
-      secret_key = "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"
+
     }
   }
 
@@ -36,4 +27,4 @@ provider "aws" {
 
   required_version = "~> 1.2"
 }
-*/
+
